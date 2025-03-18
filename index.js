d7855f0c0081e4f37624b7fc8cf6592b47a9a40e
Dat Le <dattl@vn.innova.com>
2025-03-19 00:03:02 +0700

    use dist for app

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+app.use(express.static('dist'))
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
@@ -34,9 +35,9 @@ let notes = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello, World!</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello, World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
